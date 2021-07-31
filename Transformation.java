751a687bc9427f9abe9c87dee464423ab9cb31a0
ampp8800 <87927435+ampp8800@users.noreply.github.com>
2021-08-01 02:42:41 +0300

    going from double to BigDecimal

--- Transformation.java
+++ Transformation.java
@@ -1,15 +1,23 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 class Transformation {
     private Object result;
+    BigDecimal maxDouble = new BigDecimal(Double.MAX_VALUE);
+    Transformation(BigDecimal data) {
 
-    Transformation(Double data) {
-
-        if (data == Double.MAX_VALUE){
+        if (data.compareTo(maxDouble) == 1) {
             this.result = "out of limits";
-        }else if (data % 1 == 0) {
-            this.result = data.intValue();
         }else{
-            this.result = Math.round(data*10000.0)/10000.0;
+            this.result = data.setScale(4, RoundingMode.HALF_UP);
+            String resultString = this.result.toString();
+
+            for (int i = 0; i<5; i++){
+                if (resultString.endsWith("0") || resultString.endsWith(".")){
+                    resultString = resultString.substring(0, resultString.length() - 1);
+                }
+            }
+            this.result = resultString;
         }
 
     }
