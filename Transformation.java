9cda1a62041d76bb804a3636e6a2ee2b739248c5
ampp8800 <87927435+ampp8800@users.noreply.github.com>
2021-07-31 23:46:16 +0300

    fixed the shit

--- Transformation.java
+++ Transformation.java
@@ -4,8 +4,8 @@ class Transformation {
     Transformation(Double data) {
         if (data % 1 == 0) {
             this.result = (int) Math.round(data);
-        }else if (data == 1/0.0 || data >= Float.MAX_VALUE){
-            this.result = "out-of-value";
+        }else if (data == 1/0.0){
+            this.result = "infiniti";
         }else{
             this.result = Math.round(data*10000.0)/10000.0;
         }
