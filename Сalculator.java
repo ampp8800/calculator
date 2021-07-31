b7318e49f2561538dff290c0fa58d13050de103a
ampp8800 <87927435+ampp8800@users.noreply.github.com>
2021-08-01 00:02:49 +0300

    fixed the shit(1)

--- Сalculator.java
+++ Сalculator.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 
 public class Сalculator {
 
@@ -35,13 +36,34 @@ public class Сalculator {
 
     }
     static Double calculation  (DataConversion data){
-        double result = 37707;
-        if (data.getOp().equals("-")){ result = data.getX() - data.getY();}
-        if (data.getOp().equals("+")){ result = data.getX() + data.getY();}
-        if (data.getOp().equals("/")){ result = data.getX() / data.getY();}
-        if (data.getOp().equals("*")){ result = data.getX() * data.getY();}
+        double result = 37;
+        BigDecimal maxDouble = new BigDecimal(Double.MAX_VALUE);
+        BigDecimal currentDouble = null;
+        if (data.getOp().equals("-")){
+            currentDouble = new BigDecimal(data.getX() - data.getY());
+            if (currentDouble.compareTo(maxDouble) == 1){
+                result = Double.MAX_VALUE;
+            }else {result = data.getX() - data.getY();}
+        }
+        if (data.getOp().equals("+")){
+            currentDouble = new BigDecimal(data.getX() + data.getY());
+            if (currentDouble.compareTo(maxDouble) == 1){
+                result = Double.MAX_VALUE;
+            }else {result = data.getX() + data.getY();}
+        }
+        if (data.getOp().equals("/")){
+            currentDouble = new BigDecimal(data.getX() / data.getY());
+            if (currentDouble.compareTo(maxDouble) == 1){
+                result = Double.MAX_VALUE;
+            }else {result = data.getX() / data.getY();}
+        }
+        if (data.getOp().equals("*")){
+            currentDouble = new BigDecimal(data.getX() * data.getY());
+            if (currentDouble.compareTo(maxDouble) == 1){
+                result = Double.MAX_VALUE;
+            }else {result = data.getX() * data.getY();}
+        }
+        System.out.println(currentDouble);
         return result;
     }
-
-
 }
