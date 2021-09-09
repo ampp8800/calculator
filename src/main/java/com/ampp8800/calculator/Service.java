373e0249e058d574277a7b36dfa4efc52eedd0ff
ampp8800 <87927435+ampp8800@users.noreply.github.com>
2021-09-09 23:02:50 +0300

    switch edits

--- src/main/java/com/ampp8800/calculator/Service.java
+++ src/main/java/com/ampp8800/calculator/Service.java
@@ -3,33 +3,31 @@ package com.ampp8800.calculator;
 public class Service {
 
     public static Warehouse definition(Data data) {
-        if (Operation.Command.REMEMBER.equals(data.getOp())) {
-            if (data.getStr() != null) {
-                return new Warehouse(RepositoryWorker.recording(data.getStr()));
-            }
+        switch (data.getOp()) {
+            case REMEMBER:
+                if (data.getStr() != null) {
+                    return new Warehouse(RepositoryWorker.recording(data.getStr()));
+                }
+            case SHOW:
+                if (data.getStr() != null) {
+                    return new Warehouse(RepositoryWorker.reading(data.getStr()));
+                }
+            case EXIT:
+                if (data.getStr() == null) {
+                    return new Warehouse("close calculator", true);
+                }
+            case REMOVE_ALL:
+                if (data.getStr() == null) {
+                    RepositoryWorker.removeAll();
+                    return new Warehouse("repository clear");
+                }
+            case HELP:
+                if (data.getStr() == null) {
+                    UserStreamProcesson.help();
+                    return new Warehouse();
+                }
+            default:
+                return null;
         }
-        if (Operation.Command.SHOW.equals(data.getOp())) {
-            if (data.getStr() != null) {
-                return new Warehouse(RepositoryWorker.reading(data.getStr()));
-            }
-        }
-        if (Operation.Command.EXIT.equals(data.getOp())) {
-            if (data.getStr() == null) {
-                return new Warehouse("close calculator", true);
-            }
-        }
-        if (Operation.Command.REMOVE_ALL.equals(data.getOp())) {
-            if (data.getStr() == null) {
-                RepositoryWorker.removeAll();
-                return new Warehouse("repository clear");
-            }
-        }
-        if (Operation.Command.HELP.equals(data.getOp())) {
-            if (data.getStr() == null) {
-                UserStreamProcesson.help();
-                return new Warehouse();
-            }
-        }
-        return null;
     }
 }
